0338d5d095f5604e0b874f113f0c50d736f04489
NinelSchneid <124437605+NinelSchneid@users.noreply.github.com>
2023-02-05 12:20:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[] { "1234", "1567", "-2", "computer science", "mos" };
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array, string[] arrayNew)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayNew[count] = array[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
